0abd1c72abc7a236b2133dad4e57d94038c8f2cd
ankur_ranjan <ankurranjanabcd@gmail.com>
2023-03-09 03:23:24 +0530

    minor fix for auth middleware

--- src/lib/middlewares/authMiddleware.ts
+++ src/lib/middlewares/authMiddleware.ts
@@ -7,19 +7,14 @@ import { errorResponse } from '../apiout';
 const verifyToken = (req: Request, res:Response , next: NextFunction):void|Response => {
   const token =
   req.body.token || req.query.token || req.headers["x-access-token"] || req.headers.authorization?.split("Bearer ")[1];
-  if (!token) {
-    return errorResponse(
-      new ValidationError('A token is required for authentication',403),
-      res
-        );
-      }
-    try {
+  if (!token) return errorResponse(new ValidationError('A token is required for authentication',403),res);
       const private_key:Secret = process.env.TOKEN_KEY as string;
+      try {
       const decoded = jwt.verify(token, private_key);
       req.user = decoded;
     } catch (err) {
       console.log(err)
-      throw new ValidationError('Invalid Token',401)
+      return errorResponse(new ValidationError('Invalid Token',401),res);
     }
     return next();
   };
